91d658ae873fdb8507eee43debb4a5015673b655
artemis_h <tranphamminhhung65@gmail.com>
2024-12-14 02:22:55 +0700

    fix(store): resolve merge conflict

--- src/stores/use-window-store.ts
+++ src/stores/use-window-store.ts
@@ -3,17 +3,11 @@ import { persist } from 'zustand/middleware';
 
 import { Window } from '@/types';
 
-<<<<<<< HEAD
 import useItemStore from './use-item-store';
 
 interface WindowStore {
   windows: Window[];
   openFolder: (id: string) => void;
-=======
-interface WindowStore {
-  windows: Window[];
-  openWindow: (type: 'FOLDER' | 'GAME') => void;
->>>>>>> new-ver
   handleFocus: (id: number) => void;
   closeWindow: (id: number) => void;
 }
@@ -22,7 +16,6 @@ const useWindowStore = create(
   persist<WindowStore>(
     (set, get) => ({
       windows: [],
-<<<<<<< HEAD
       openFolder: (id: string) => {
         const item = useItemStore.getState().items.find(item => item.id === id);
         if (!item || item.type !== 'FOLDER') return;
@@ -43,19 +36,6 @@ const useWindowStore = create(
         };
         set({
           windows: [...windows, newWindow],
-=======
-      openWindow: (type: 'FOLDER' | 'GAME') => {
-        const windows = get().windows;
-        const newWindow = {
-          id: Date.now(),
-          type,
-        };
-        set({
-          windows: [
-            ...windows,
-            { ...newWindow, zIndex: getNextZIndex(windows) },
-          ],
->>>>>>> new-ver
         });
       },
       handleFocus: (id: number) => {
@@ -80,10 +60,6 @@ const useWindowStore = create(
 );
 
 const getNextZIndex = (windows: Window[]) =>
-<<<<<<< HEAD
   Math.max(0, ...windows.map(w => w.zIndex)) + 1;
-=======
-  Math.max(...windows.map(w => w.zIndex)) + 1;
->>>>>>> new-ver
 
 export default useWindowStore;
